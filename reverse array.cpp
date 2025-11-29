2b17a908ee1fa73915e9ef1a1522ccd1c5a791ad
tejdajit <tejdajityengkhom@gmail.com>
2025-11-30 00:35:28 +0530

    Add files via upload

---
+++ reverse array.cpp
@@ -0,0 +1,51 @@
+/*Problem 1: Reverse the Array (Neha’s Diary Entries)
+🧩 Problem Story:
+
+Neha keeps a diary of her daily exercise times (in minutes).
+She wants to review her progress, but she prefers to see the latest day’s record first.
+
+Write a program to help Neha reverse the order of her recorded exercise times.
+
+✅ Task:
+
+Given n numbers representing Neha’s daily exercise time, print them in reverse order.
+
+🧠 Input Format:
+
+The first line contains an integer n — the number of days.
+
+The second line contains n space-separated integers representing exercise times.
+Output Format:
+
+Print the exercise times in reverse order on a single line separated by spaces.
+
+🧩 Example 1:
+
+Input
+5
+10 20 30 40 50
+
+Output
+50 40 30 20 1*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[1000];  
+
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+
+    for (int i = n - 1; i >= 0; i--) {
+        cout << arr[i];
+        if (i != 0) cout << " ";
+    }
+
+    return 0;
+}
